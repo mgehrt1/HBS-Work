7d12eafd1e237e9b2c95f49130da912b615b5429
mgehrt1 <114352987+mgehrt1@users.noreply.github.com>
2023-05-18 08:42:50 -0500

    Added C# Challenges

---
+++ C# Challenges/Equality Check/Equality Check/Program.cs
@@ -0,0 +1,8 @@
+﻿static bool CheckEquality<A, B>(A a, B b) {
+    return Equals(a, b);
+}
+
+// tests
+Console.WriteLine(CheckEquality(1, true));
+Console.WriteLine(CheckEquality(0, "0"));
+Console.WriteLine(CheckEquality(1, 1));
